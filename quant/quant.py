32ff324aca702a485e3fdfbe2de3865e37339c17
Jacob Kaufmann <jacobkaufmann18@gmail.com>
2018-05-21 18:53:13 -0400

    Add files via upload

--- quant/quant.py
+++ quant/quant.py
@@ -1,10 +1,103 @@
-import numpy as np
+from __future__ import absolute_import, division, print_function
+
+import os
+import matplotlib.pyplot as plt
+
 import tensorflow as tf
-import tflearn
-import pandas as pd
+import tensorflow.contrib.eager as tfe
 
-from data import process_equity_data
+tf.enable_eager_execution()
 
-stock = 'AAPL'
-data = process_equity_data.read_data(stock)
-print(data)
+def parse_csv(line):
+  example_defaults = [[0.], [0.], [0.], [0.], [0.], [0.], [0]]  # sets field types
+  parsed_line = tf.decode_csv(line, example_defaults)
+  # First 6 fields are features, combine into single tensor
+  features = tf.reshape(parsed_line[:-1], shape=(6,))
+  # Last field is the label
+  label = tf.reshape(parsed_line[-1], shape=())
+  return features, label
+
+def loss(model, x, y):
+  y_ = model(x)
+  return tf.losses.sparse_softmax_cross_entropy(labels=y, logits=y_)
+
+def grad(model, inputs, targets):
+  with tf.GradientTape() as tape:
+    loss_value = loss(model, inputs, targets)
+  return tape.gradient(loss_value, model.variables)
+
+train_dataset = tf.data.TextLineDataset('data/data/equities/post-processed/train.csv')
+train_dataset = train_dataset.skip(1)             # skip the first header row
+train_dataset = train_dataset.map(parse_csv)      # parse each row
+train_dataset = train_dataset.shuffle(buffer_size=1000)  # randomize
+train_dataset = train_dataset.batch(32)
+
+# View a single example entry from a batch
+features, label = iter(train_dataset).next()
+print("example features:", features[0])
+print("example label:", label[0])
+
+model = tf.keras.Sequential([
+  tf.keras.layers.Dense(10, activation="relu", input_shape=(6,)),  # input shape required
+  tf.keras.layers.Dense(20, activation="relu"),
+  tf.keras.layers.Dense(30, activation="relu"),
+  tf.keras.layers.Dense(20, activation="relu"),
+  tf.keras.layers.Dense(10, activation="relu"),
+  tf.keras.layers.Dense(2)
+])
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.005)
+
+# keep results for plotting
+train_loss_results = []
+train_accuracy_results = []
+
+num_epochs = 201
+
+for epoch in range(num_epochs):
+  epoch_loss_avg = tfe.metrics.Mean()
+  epoch_accuracy = tfe.metrics.Accuracy()
+
+  # Training loop - using batches of 32
+  for x, y in train_dataset:
+    # Optimize the model
+    grads = grad(model, x, y)
+    optimizer.apply_gradients(zip(grads, model.variables),
+                              global_step=tf.train.get_or_create_global_step())
+
+    # Track progress
+    epoch_loss_avg(loss(model, x, y))  # add current batch loss
+    # compare predicted label to actual label
+    epoch_accuracy(tf.argmax(model(x), axis=1, output_type=tf.int32), y)
+
+  # end epoch
+  train_loss_results.append(epoch_loss_avg.result())
+  train_accuracy_results.append(epoch_accuracy.result())
+
+  print("Epoch {:03d}: Loss: {:.3f}, Accuracy: {:.3%}".format(epoch, epoch_loss_avg.result(), epoch_accuracy.result()))
+
+fig, axes = plt.subplots(2, sharex=True, figsize=(12, 8))
+fig.suptitle('Training Metrics')
+
+axes[0].set_ylabel("Loss", fontsize=14)
+axes[0].plot(train_loss_results)
+
+axes[1].set_ylabel("Accuracy", fontsize=14)
+axes[1].set_xlabel("Epoch", fontsize=14)
+axes[1].plot(train_accuracy_results)
+
+plt.show()
+
+test_dataset = tf.data.TextLineDataset('data/data/equities/post-processed/test.csv')
+test_dataset = test_dataset.skip(1)             # skip header row
+test_dataset = test_dataset.map(parse_csv)      # parse each row with the funcition created earlier
+test_dataset = test_dataset.shuffle(1000)       # randomize
+test_dataset = test_dataset.batch(32)           # use the same batch size as the training set
+
+test_accuracy = tfe.metrics.Accuracy()
+
+for (x, y) in test_dataset:
+  prediction = tf.argmax(model(x), axis=1, output_type=tf.int32)
+  test_accuracy(prediction, y)
+
+  print("Test set accuracy: {:.3%}".format(test_accuracy.result()))
